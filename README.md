e970635b6471154ec2cd56d94040062d1e6a51de
Ký sự Kỹ Sư <87556305+KysuKysu@users.noreply.github.com>
2021-08-12 10:22:28 +0700

    code for fun

---
+++ README.md
@@ -0,0 +1,5 @@
+# Tặng những ai đam mê lập trình - phiên bản edit 🤭
+
+
+[![](https://img.shields.io/badge/Facebook-1877F2?style=for-the-badge&logo=facebook&logoColor=white)](https://www.facebook.com/programming4thefuture)
+
---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Bạn gì ơi!',
+    introDesc: `Bạn đã code chưa?`,
+    btnIntro: 'Code',
+    title: 'Bạn có đam mê code không nào 😙',
+    desc: 'Không trả lời hoặc thoát ra, tức là bạn có đam mê code đấy nhé',
+    btnYes: 'Thích code qtqd <33',
+    btnNo: 'Không thích code :3',
+    question:'Trên thế giới có biết bao nhiêu công việc mà sao em lại đam mê code <3',
+    btnReply: 'Send me <3',
+    reply: 'Đam mê thì code mà không đam mê thì code <33333333',
+    mess: 'Mình biết mà 🥰. Bạn cũng đam mê code như mình nè 😘😘',
+    messDesc: 'Nhớ code hàng ngày bạn nhé.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/programming4thefuture' 
+}
